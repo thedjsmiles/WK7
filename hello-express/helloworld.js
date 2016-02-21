8f360d160f3c1bb98af8cf80b87359c11456062c
Donny M <morgando@oregonstate.edu>
2016-02-21 06:05:19 -0500

    added changes

--- hello-express/helloworld.js
+++ hello-express/helloworld.js
@@ -7,6 +7,7 @@ app.set('port', 3000);
 app.get('/',function(req,res){
   res.type('text/plain');
   res.send('Welcome to the main page!');
+  res.send(Math.random());
 });
 
 app.get('/other-page',function(req,res){
