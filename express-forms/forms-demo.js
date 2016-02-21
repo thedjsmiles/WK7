e111d7c735f1529413ffccdf0cf2bad41f16ec5c
Donny M <morgando@oregonstate.edu>
2016-02-21 18:08:37 -0500

    more changes

--- express-forms/forms-demo.js
+++ express-forms/forms-demo.js
@@ -4,7 +4,7 @@ var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
@@ -43,17 +43,17 @@ app.get('/get-loopback-improved',function(req,res){
   res.render('get-loopback-improved', context);
 });
 
-app.post('/post-loopback', function(req,res){
+app.post('/post-loopback', function(req, res){
   var qParams = [];
-  for (var p in req.body){
-    qParams.push({'name':p,'value':req.body[p]})
+  for(var p in req.body){
+    qParams.push({'name':p, 'value': req.body[p]});
   }
   console.log(qParams);
   console.log(req.body);
   var context = {};
   context.dataList = qParams;
   res.render('post-loopback', context);
-}); 
+});
 
 app.use(function(req,res){
   res.status(404);
