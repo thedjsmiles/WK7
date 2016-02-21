d9ae9d77c61803cbfa511ec72ecca9ea4d6fe9df
Donny M <morgando@oregonstate.edu>
2016-02-21 16:49:37 -0500

    added code to forms-demo.js

---
+++ express-forms/forms-demo.js
@@ -0,0 +1,34 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res){
+  res.render('home') //We can omit the .handlebars extension as we do below
+});
+
+app.get('/show-data',function(req,res){
+  var context = {};
+  context.sentData = req.query.myData;
+  res.render('show-data', context);
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
