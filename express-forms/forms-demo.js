d924acce9d7139e487ea8c4f0dd546038aa70d33
Donny M <morgando@oregonstate.edu>
2016-02-21 17:56:14 -0500

    added post-loopback.handlebars

--- express-forms/forms-demo.js
+++ express-forms/forms-demo.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -37,6 +41,18 @@ app.get('/get-loopback-improved',function(req,res){
   var context = {};
   context.dataList = qParams;
   res.render('get-loopback-improved', context);
+});
+
+app.post('/post-loopback', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  console.log(qParams);
+  console.log(req.body);
+  var context = {};
+  context.dataList = qParams;
+  res.render('post-loopback', context);
 });
 
 app.use(function(req,res){
